b8a352e4ddeb5bcec791cfdd2e111907c27400f9
DuzyBuzz <wenweng003@gmail.com>
2025-08-21 22:54:00 +0800

    added expenses service, modal, and component

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
 export class AdminComponent {
+  isSidebarOpen = false;
+  constructor(private router: Router) { }
 
+    logout() {
+    localStorage.removeItem('user'); // clear session
+    this.router.navigate(['/login']); // redirect to login
+  }
 }
